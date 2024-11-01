89d5c12a64c4e3f4d4c4e50159d98268e6f17fb8
Carsten Lohmann <clohmann@gmx.net>
2024-11-01 16:11:47 +0000

    Initial setup

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace WebDev.Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnsiConsole.MarkupLine("Hello world!");
+        }
+    }
+}
