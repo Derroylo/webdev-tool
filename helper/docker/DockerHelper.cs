31888e1d50b14635d8591539bd579262b85b47dd
Carsten Lohmann <carsten.lohmann@connectiv.de>
2025-10-11 15:40:57 +0000

    Fixed an error where the detection of running devcontainers would always return 1

--- helper/docker/DockerHelper.cs
+++ helper/docker/DockerHelper.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Spectre.Console;
 using WebDev.Tool.Helper.Internal.Config.Sections;
 using YamlDotNet.Serialization.NamingConventions;
 
@@ -15,7 +16,11 @@ namespace WebDev.Tool.Helper.Docker
 
             var result = ExecCommand.Exec("docker ps -q -f status=running -f name=" + name);
 
-            return result.Trim().Split('\n').ToList();
+            if (result.Trim().Length > 0) {
+                containers = result.Trim().Split('\n').ToList();
+            }
+
+            return containers;
         }
 
         public static void StopContainer(string name)
