2b4c6c474bfbca0830979a85c0bc9d2b5cc717ed
Carsten Lohmann <carsten.lohmann@connectiv.de>
2026-02-05 00:07:55 +0000

    Rewrote the mysql update command so that you can provide multiple services to be updated or selecting them via  a multiselect. Also add a fallback if the command docker-compose does not exist

--- commands/mysql/MysqlPullCommand.cs
+++ commands/mysql/MysqlPullCommand.cs
@@ -5,7 +5,6 @@ using Spectre.Console.Cli;
 using System;
 using System.Linq;
 using WebDev.Tool.Helper.Docker;
-using System.IO;
 using WebDev.Tool.Helper.Internal;
 
 namespace WebDev.Tool.Commands.Mysql
@@ -14,6 +13,10 @@ namespace WebDev.Tool.Commands.Mysql
     {
         public class Settings : LogCommandSettings
         {
+            [CommandArgument(0, "[Service]")]
+            [Description("The name of the service to pull")]
+            [DefaultValue("")]
+            public string Service { get; set; }
         }
 
         public override int Execute(CommandContext context, Settings settings)
@@ -22,45 +25,113 @@ namespace WebDev.Tool.Commands.Mysql
             
             // Check if mysql service exists in docker-compose
             var services = _dockerComposeHelper.GetServices(_dockerComposeHelper.GetFile());
-            if (!services.ContainsKey("mysql")) {
+            var mysqlServices = services.Where(s => s.Value.ContainsKey("image") && s.Key.StartsWith("mysql")).Select(s => s.Key).ToArray<string>();
+
+            if (mysqlServices.Length == 0) {
                 AnsiConsole.MarkupLine("[red]No MySQL service found in docker-compose.yml[/]");
                 return 1;
             }
 
-            var image = services["mysql"]["image"];
+            AnsiConsole.MarkupLine("Updating MySQL service: " + settings.Service);
+            if (string.IsNullOrEmpty(settings.Service) && mysqlServices.Length == 1) {
+                settings.Service = mysqlServices[0];
+            } else if (string.IsNullOrEmpty(settings.Service)) {
+                settings.Service = SelectServicesToUpdate(mysqlServices);
+            }
 
-            if (string.IsNullOrEmpty(image)) {
-                AnsiConsole.MarkupLine("[red]No image found for MySQL service in docker-compose.yml[/]");
+            var selectedServices = settings.Service.Split(',');
+            if (selectedServices.Length == 0) {
+                AnsiConsole.MarkupLine("[red]No MySQL service selected[/]");
                 return 1;
             }
 
-            if (image.StartsWith("mysql:")) {
-                AnsiConsole.MarkupLine("[red]Only custom database images can be updated.[/]");
-                return 1;
-            }
-
-            if (!AnsiConsole.Confirm("Do you really want to update the database image? All changes to the current database will be lost.", false))
-            {
+            if (!AnsiConsole.Confirm("Do you really want to update the selected database image(s)? All changes to the database will be lost.", false)) {
                 return 0;
             }
 
             var composeFile = _dockerComposeHelper.GetFile();
-            
-            AnsiConsole.MarkupLine("Stopping MySQL container...");
-            _execCommand.ExecWithDirectOutput("docker-compose -f " + composeFile + " stop mysql");
 
-            AnsiConsole.MarkupLine("Removing MySQL container...");
-            _execCommand.ExecWithDirectOutput("docker-compose -f " + composeFile + " rm -f mysql");
+            // Determine whether "docker-compose" or "docker compose" command should be used
+            string dockerComposeCmd;
 
-            AnsiConsole.MarkupLine("Pulling latest MySQL image...");
-            _execCommand.ExecWithDirectOutput("docker-compose -f " + composeFile + " pull mysql");
+            // Try to find out which docker compose command is available
+            try
+            {
+                var result = _execCommand.Exec("which docker-compose");
+                if (result.Contains("docker-compose"))
+                {
+                    dockerComposeCmd = "docker-compose";
+                }
+                else
+                {
+                    // Fallback: check `docker compose` by checking if `docker` is present and `docker compose version` works
+                    var dockerResult = _execCommand.Exec("docker compose version");
+                    if (dockerResult.Contains("docker compose"))
+                    {
+                        dockerComposeCmd = "docker compose";
+                    }
+                    else
+                    {
+                        AnsiConsole.MarkupLine("[red]Neither 'docker-compose' nor 'docker compose' found on this system![/]");
+                        return 1;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                AnsiConsole.MarkupLine($"[red]Error checking docker compose command: {ex.Message}[/]");
+                return 1;
+            }
 
-            AnsiConsole.MarkupLine("Starting MySQL container...");
-            _execCommand.ExecWithDirectOutput("docker-compose -f " + composeFile + " -p " + _dockerHelper.GetProjectName() + " up -d mysql");
+            foreach (var service in selectedServices) {
+                if (!mysqlServices.Contains(service)) {
+                    AnsiConsole.MarkupLine("[red]Service " + service + " not found in docker-compose.yml[/]");
+                    return 1;
+                }
 
-            AnsiConsole.MarkupLine("[green]MySQL container has been updated successfully![/]");
+                if (!services[service].ContainsKey("image")) {
+                    AnsiConsole.MarkupLine("[red]No image found for service " + service + " in docker-compose.yml[/]");
+                    return 1;
+                }
+
+                var image = services[service]["image"];
+
+                if (string.IsNullOrEmpty(image)) {
+                    AnsiConsole.MarkupLine("[red]No image found for MySQL service in docker-compose.yml[/]");
+                    return 1;
+                }
+
+                if (image.StartsWith("mysql:")) {
+                    AnsiConsole.MarkupLine("[red]Only custom database images can be updated.[/]");
+                    return 1;
+                }
+
+                AnsiConsole.MarkupLine("Stopping " + service + " container...");
+                _execCommand.ExecWithDirectOutput(dockerComposeCmd + " -f " + composeFile + " stop " + service);
+
+                AnsiConsole.MarkupLine("Removing " + service + " container...");
+                _execCommand.ExecWithDirectOutput(dockerComposeCmd + " -f " + composeFile + " rm -f " + service);
+
+                AnsiConsole.MarkupLine("Pulling latest " + service + " image...");
+                _execCommand.ExecWithDirectOutput(dockerComposeCmd + " -f " + composeFile + " pull " + service);
+
+                AnsiConsole.MarkupLine("Starting " + service + " container...");
+                _execCommand.ExecWithDirectOutput(dockerComposeCmd + " -f " + composeFile + " -p " + _dockerHelper.GetProjectName() + " up -d " + service);
+            }
+
+            AnsiConsole.MarkupLine("[green]" + string.Join(", ", selectedServices) + " container(s) have been updated successfully![/]");
 
             return 0;
+        }
+
+        private string SelectServicesToUpdate(string[] services)
+        {
+            var selected = AnsiConsole.Prompt(
+                new MultiSelectionPrompt<string>()
+                .Title("Which [green]services[/] do you want to update?")
+                .AddChoices(services));
+
+            return string.Join(",", selected.ToArray<string>());
         }
     }
 }
